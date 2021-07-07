cedf3e0cf9617b9e81bd20bfd4e308fce07d37fe
santhosh-cloud <coe19b041@iiitdm.ac.in>
2021-07-07 17:11:28 +0530

    Second commit: test message

--- pages/cowin.js
+++ pages/cowin.js
@@ -9,7 +9,7 @@ function cowin() {
         <title>2nd Page</title>
       </Head>
       <Layout title="2ndpage">
-        <h2 className="title">This is Cowin Portal(Updated)</h2>
+        <h2 className="title">This is Cowin Portal(Updated-2)</h2>
       </Layout>
     </>
   );
